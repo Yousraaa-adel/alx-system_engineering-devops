11db1ed8ea1d7e986fb8112d55b31c13c1dd0f56
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-08 18:42:43 +0300

    API 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -16,26 +16,28 @@ if __name__ == "__main__":
     user_response = requests.get(user_url)
     user_data = user_response.json()
     employee_name = user_data["name"]
+    employee_username = user_data["username"]
 
     todos_url = f"https://jsonplaceholder.typicode.com/todos?userId="\
         f"{employee_id}"
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
+    # print(todos_data)
+    # print(str(employee_id))
+    # print(employee_name)
+    # print(user_data["username"])
 
     total_tasks = len(todos_data)
     completed_tasks = [task for task in todos_data if task['completed']]
     number_of_done_tasks = len(completed_tasks)
 
-    with open("USER_ID.csv", "w") as file:
-        writer = csv.writer(file)
-        writer.writerow([
-            "USER_ID",
-            "USERNAME",
-            "TASK_COMPLETED_STATUS",
-            "TASK_TITLE"
-            ])
+    with open("USER_ID.csv", "w", newline="") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
 
         for task in todos_data:
-            writer.writerow(
-                [employee_id, employee_name, task["completed"], task["title"]]
-            )
+            writer.writerow([
+                employee_id,
+                employee_username,
+                task["completed"],
+                task["title"]
+            ])
